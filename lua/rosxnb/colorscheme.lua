6096c554dd5a8f08d0dfdd61560ad9e86b3f2ca2
Roshan Bharati <rosxnb@gmail.com>
2024-10-23 21:59:43 +0545

    new colorscheme and disabe auto hints

--- lua/rosxnb/colorscheme.lua
+++ lua/rosxnb/colorscheme.lua
@@ -1,30 +1,52 @@
 local M = {
-    "rebelot/kanagawa.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
 }
 
 function M.config()
-    vim.api.nvim_set_hl(  0,  "NormalFloat",  { bg = "none" }  )
-    vim.api.nvim_set_hl(  0,  "Normal",       { bg = "none" }  )
-    vim.api.nvim_set_hl(  0,  "NormalNC",     { bg = "none" }  )
-    vim.api.nvim_set_hl(  0,  "NonText",      { bg = "none" }  )
-    vim.api.nvim_set_hl(  0,  "SignColumn",   { bg = "none" }  )
+    -- vim.api.nvim_set_hl(  0,  "NormalFloat",  { bg = "none" }  )
+    -- vim.api.nvim_set_hl(  0,  "Normal",       { bg = "none" }  )
+    -- vim.api.nvim_set_hl(  0,  "NormalNC",     { bg = "none" }  )
+    -- vim.api.nvim_set_hl(  0,  "NonText",      { bg = "none" }  )
+    -- vim.api.nvim_set_hl(  0,  "SignColumn",   { bg = "none" }  )
 
-    require("kanagawa").setup {
-        transparent = true,          -- do not set background color
-        dimInactive = false,          -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-        colors = {                   -- add/modify theme and palette colors
-            theme = {
-                all = {
-                    ui = { bg_gutter = "none" }
-                }
-            },
+    require("catppuccin").setup({
+        flavour = "auto", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+            light = "latte",
+            dark = "mocha",
         },
-    }
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+            enabled = false, -- dims the background color of inactive window
+            shade = "dark",
+            percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = true, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+            comments = { "italic" }, -- Change the style of comments
+            conditionals = { "italic" },
+            loops = {},
+            functions = {},
+            keywords = {},
+            strings = {},
+            variables = {},
+            numbers = {},
+            booleans = {},
+            properties = {},
+            types = {},
+            operators = {},
+            -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+    })
 
-    vim.cmd.colorscheme "kanagawa"
+    vim.cmd.colorscheme "catppuccin"
 end
 
 return M
--- lua/rosxnb/lspconfig.lua
+++ lua/rosxnb/lspconfig.lua
@@ -32,9 +32,9 @@ end
 M.on_attach = function(client, bufnr)
     lsp_keymaps(bufnr)
 
-    if client.supports_method "textDocument/inlayHint" then
-        vim.lsp.inlay_hint.enable(true, { bufnr })
-    end
+    -- if client.supports_method "textDocument/inlayHint" then
+    vim.lsp.inlay_hint.enable(false, { bufnr })
+    -- end
 end
 
 function M.common_capabilities()
