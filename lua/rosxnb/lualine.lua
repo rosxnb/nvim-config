e770dcacc824ef8afb0022846a5961d63a9c303f
Roshan Bharati <rosxnb@gmail.com>
2025-02-20 14:16:40 +0545

    Display time in lualine

--- lua/rosxnb/lualine.lua
+++ lua/rosxnb/lualine.lua
@@ -39,7 +39,11 @@ function M.config()
                 { "progress", separator = " ", padding = { left = 1, right = 0 } },
                 { "location", padding = { left = 0, right = 1 } },
             },
-            lualine_z = {},
+            lualine_z = {
+                function()
+                    return " " .. os.date("%R")
+                end,
+            },
         },
         extensions = { "quickfix", "man", "fugitive" },
     }
